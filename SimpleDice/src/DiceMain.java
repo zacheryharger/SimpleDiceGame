c125af7a7ecdfb59d98f5883b67b041e4129f87b
Zach <Zach@10.0.69.120>
2019-09-09 11:44:13 -0500

    Starting Code with Game added

--- SimpleDice/src/DiceMain.java
+++ SimpleDice/src/DiceMain.java
@@ -2,8 +2,9 @@
 public class DiceMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Let's play!");
+	      Game g = new Game();
+	      g.play();
 	}
 
 }
---
+++ SimpleDice/src/Game.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Game {
+  
+   @SuppressWarnings("resource")
+public void play() {
+   
+      // get input from user
+      Scanner scnr = new Scanner(System.in);
+      System.out.print("How much do you want to bet? ");                                                 
+      int enteredBet = scnr.nextInt(); 
+      System.out.print("What number do you choose ? ");
+      int enteredNum = scnr.nextInt(); 
+   
+      // generate the die roll 
+      Random randGen = new Random();
+      int dieNum  =  randGen.nextInt(6) + 1;
+      System.out.println("The die is a " + dieNum);
+   
+      // write the rest of your code below
+        
+   }
+}
